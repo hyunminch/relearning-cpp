e713ff5fb8d414ef1be60ae0bdc7558146a46b39
Hyun Min Choi <hyunmin.personal@gmail.com>
2017-11-11 15:58:45 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 
 #include "vector.h"
+#include "node.h"
 
 void VectorMain() {
   int n = 0;
@@ -17,8 +19,32 @@ void VectorMain() {
   std::cout << "Sum of elements in vector = " << sum << std::endl;
 }
 
-int main(void) {
-  VectorMain();
+void PointerMain() {
+  using namespace nitrogenice;
+
+  Node<int> n3(5);
+  n3.set_left(std::make_shared<Node<int>>(3));
+  n3.set_right(std::make_shared<Node<int>>(4));
+
+  std::vector<int> traversed = n3.traverse();
+  for (auto e: traversed)
+    std::cout << e << std::endl;
+}
+
+int main() {
+  std::string main_mode;
+
+  std::cout << "What do you want to run?\n";
+  std::cout << "[Currently supported modes: {vector, pointer}\n";
+  std::cin >> main_mode;
+
+  if (main_mode == "vector") {
+    VectorMain();
+  } else if (main_mode == "pointer") {
+    PointerMain();
+  } else {
+    std::cout << "Not supported yet :(\n";
+  }
 
   return 0;
 }
